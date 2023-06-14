9aed0a56f46cf7ec8b605b5820ea6e3e413869e4
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2023-06-14 19:18:54 -0300

    Repondo projeto

---
+++ main/main.js
@@ -0,0 +1,67 @@
+var btn = document.getElementById('run').addEventListener('click', submit)
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+
+        var btn = document.querySelector("#run");
+
+        btn.click();
+
+    }
+});
+function submit() {
+    //ocultar página 1 e exibir página 2
+    let p1 = document.getElementById('page1')
+    let p2 = document.getElementById('page2')
+    let header = document.getElementById('header')
+    let footer = document.getElementById('footer')
+    let text1 = document.getElementById('codeHtml')
+    let text2 = document.getElementById('codeJs')
+
+    p1.style.display = "none"
+    header.style.display = "none"
+    footer.style.display = "none"
+    p2.style.display = "flex"
+    //inserir código em HTML
+    let codeHtml = document.getElementById('codeHtml').value
+    p2.innerHTML = codeHtml
+    //inserir o código em JS
+    let codeJs = document.getElementById('codeJs').value
+    document.getElementById('jsUser').innerHTML = codeJs
+
+    document.getElementById("link").setAttribute("href", "../style/style2.css")
+
+
+    var button = document.createElement("button")
+    button.addEventListener("click", reset)
+    button.appendChild(document.createTextNode("RESET"))
+    p2.appendChild(button)
+}
+
+
+
+
+function reset() {
+    //ocultar página 1 e exibir página 2
+    let p1 = document.getElementById('page1')
+    let p2 = document.getElementById('page2')
+    let header = document.getElementById('header')
+    let footer = document.getElementById('footer')
+
+
+    p1.style.display = "flex"
+    header.style.display = "flex"
+    footer.style.display = "flex"
+    p2.style.display = "none"
+
+    document.getElementById("link").setAttribute("href", "../style/style2.css")
+    location.reload()
+    /*
+    let codeJs = document.getElementById('codeJs').value
+    var tagScript = document.createElement("script").setAttribute("type", "text/javascript")
+
+    var textScript = document.createTextNode(codeJs)
+
+    tagScript.appendChild(textScript)
+    document.body.appendChild(tagScript) 
+    */
+}
